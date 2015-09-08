4697dbfe01cff0f45431cac9ef6f03f831d96de2
ukulkom <OMAKR.KULKARNI@PEARSON.COM>
2015-09-08 11:44:18 -0400

    Added DAL year authentication file

---
+++ RESTDAL/UserDAL/src/main/java/com/user/dal/AuthenticationDAL.java
@@ -0,0 +1,13 @@
+package com.user.dal;
+
+/**
+ * Created by ukulkom on 9/8/15.
+ */
+public class AuthenticationDAL {
+
+
+    public void authenticateUser()
+    {
+        System.out.println("Authenticating the user in DAL layer");
+    }
+}
